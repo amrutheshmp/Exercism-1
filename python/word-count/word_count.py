593a0fc3a497acdbd75b1acaffda0eaa026f215c
Himanshu Kumar <himanshukumar660@gmail.com>
2017-10-24 12:07:46 +0530

    Updated Word_cnt.py
    Updated the file as per the requirements.

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,12 +1,9 @@
 def word_count(s):
-    import re
-    d = {}
-    split = sorted(re.findall(r'[a-zA-Z0-9]+', s.lower()))
-    d[split[0]] = 1
-    for i in range(0, len(split) - 1):
-        if split[i] == split[i + 1]:
-            d[split[i]] = d[split[i]] + 1
+    mydict = {}
+    word_lst = s.split(' ')
+    for word in word_lst:
+        if not word in mydict:
+            mydict[word] = 1
         else:
-            d[split[i + 1]] = 1
-    return d
-    pass
+            mydict[word]+=1
+    return mydict

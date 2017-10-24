3e00f0282cc439bcc60bc163771cd557a66697c3
Vishnu Dev TJ <vishnudevtj@gmail.com>
2017-10-24 09:30:45 +0530

    Some code Edit

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,13 +1,2 @@
 def word_count(s):
-    import re
-    d = {}
-    split = sorted(re.findall(r'[a-zA-Z0-9]+',s.lower()))
-    d[split[0]] = 1
-    for i in range(0,len(split)-1):
-        if split[i]==split[i+1]:
-            d[split[i]] = d[split[i]] + 1
-        else:
-            d[split[i+1]] = 1
-    return d        
     pass
-
